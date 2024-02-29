2f7608dd7a2f231a5591886ca07d665c8cbeca59
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2024-02-29 18:39:30 +0530

    user model created

--- src/api/utils/db.ts
+++ src/api/utils/db.ts
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
-const connection = {};
+interface Connection {
+  isConnected: boolean;
+}
+const connection = {
+  isConnected: false,
+};
 
 async function connect() {
   if (connection.isConnected) {
@@ -8,7 +13,10 @@ async function connect() {
     return;
   }
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(
+      process.env.MONGODB_URI ||
+        "mongodb+srv://usa44052:ishan@cluster0.56ejkov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     connection.isConnected = true;
     console.log("connected");
   } catch (error) {
